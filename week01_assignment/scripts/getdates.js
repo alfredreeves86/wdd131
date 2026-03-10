1783a9a6c6295c513d3f84aeea971b8983bcb072
Alfred Kamisese <97272123+alfredreeves86@users.noreply.github.com>
2026-03-09 19:28:20 -0700

    More adjustment to css file and create js file

---
+++ week01_assignment/scripts/getdates.js
@@ -0,0 +1,3 @@
+const year = new Date().getFullYear();
+document.getElementById("copyright").textContent = `© ${year} Alfred Kamisese`;
+document.getElementById("lastModified").textContent = `Last Modified: ${document.lastModified}`;
